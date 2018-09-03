360b95db0d3a419a174a38b90e6e28fda2ef78cb
Akshay Sreekumar <akshaysreekumar1997@gmail.com>
2018-09-03 17:41:54 +0530

    Update csv_concat.py

--- csv_concat.py
+++ csv_concat.py
@@ -1,18 +1,15 @@
 import csv
-import os
 import numpy as np
 a = np.empty((1,23))
-patient='01_'
+patient='01'
 hours = 42
-for i in range(hours):
-    fname = 'chb'+patient'+str(i)+'.csv'
+for i in range(hours+1):
+    fname = './chb_'+patient+'/chb_'+patient+'_'+str(i)+'.csv'
     with open(fname) as myFile:
         reader = csv.reader(myFile)
         for row in reader:
             a = np.vstack((a,row))
-a = np.delete(a, 0,0)
-print a
-
+a = np.delete(a,0,0)
 myFile = open('final.csv', 'w')
 with myFile:
    writer = csv.writer(myFile)
