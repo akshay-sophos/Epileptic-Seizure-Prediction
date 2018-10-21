0f0d840c914f6f3b76dc10bffc6e70b2ce588c95
udayadara28 <42904923+udayadara28@users.noreply.github.com>
2018-10-21 10:32:00 +0530

    Made it robust

--- save_files.py
+++ save_files.py
@@ -14,14 +14,17 @@ while j<(numcols-1):
  while i<numrows:
   if ((i == 0)&(j == 0)):
    X = dat[i:i+256,j].transpose()
-   y = dat[i,23]
+   yd = dat[i,-2]
+   yp = dat[i,-1]
   else :
    X = np.vstack((X,dat[i:i+256,j].transpose()))
-   y = np.vstack((y,dat[i,23]))
+   yd = np.vstack((y,dat[i,-2]))
+   yp = np.vstack((y,dat[i,-1]))
   i = i+256
  j = j+1
  print(j)
 print("Saving")
 
 np.save('./X'+title+'.npy', X)
-np.save('./Y'+title+'.npy', y)
+np.save('./Yp'+title+'.npy', yp)
+np.save('./Yd'+title+'.npy', yd)
