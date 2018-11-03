2b57b7f6fd921c400b8a55fc5f4652d459a9f8c7
Akshay Sreekumar <akshaysreekumar1997@gmail.com>
2018-11-03 14:01:50 +0530

    Working
    Confusion Matrix remaining

--- onn.py
+++ onn.py
@@ -9,17 +9,16 @@ dat = genfromtxt('chb_01_4.csv',dtype =(float), delimiter=',')
 print ('.............Data Loaded...............')
 
 learning_rate = 0.003
-NUM_EPISODES = 10
-cost_plot_test =[]
-accuracy_plot_test=[]
+THRESHOLD = 0.5
+NUM_EPISODES = 6
 accuracy_plot_train=[]
 cost_plot_train=[]
 
 # number of neurons in each layer
 input_num_units = 23
-hidden_num_units1 = 20
-hidden_num_units2 = 20
-hidden_num_units3 = 20
+hidden_num_units1 = 30
+hidden_num_units2 = 30
+hidden_num_units3 = 30
 output_num_units = 1
 seed = 10
 
@@ -36,6 +35,7 @@ optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 predicted = tf.nn.sigmoid(output_layer)
 correct_pred = tf.equal(tf.round(predicted), tf_exp_y)
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+#cm = tf.confusion_matrix(tf_exp_y,predicted>THRESHOLD)
 print ('...................NN created...................')
 
 # Splitting the dataset into the Training set and Test set
@@ -43,50 +43,25 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(dat[:,:-1], dat[:,-1], test_size = 0.3, random_state = 0)
 
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    y_train = y_train.reshape(y_train.size,1)
-    epoch = 0
-    for epoch in range(NUM_EPISODES):
-        _,a,c= sess.run([optimizer,accuracy,cost], {tf_x: X_train, tf_exp_y:y_train})
-        cost_plot_train = np.append(cost_plot_train,c)
-        accuracy_plot_train = np.append(accuracy_plot_train,a)
-        print(epoch, "T_Cost:%.4f" %c,"Accuracy:%.4f" %a)
-    # cm = tf.confusion_matrix(y_train,predicted)
+	sess.run(tf.global_variables_initializer())
+	y_train = y_train.reshape(y_train.size,1)
+	epoch = 0
+	for epoch in range(NUM_EPISODES):
+		_,a,c= sess.run([optimizer,accuracy,cost], {tf_x: X_train, tf_exp_y:y_train})
+		cost_plot_train = np.append(cost_plot_train,c)
+		accuracy_plot_train = np.append(accuracy_plot_train,a)
+		print(epoch, "T_Cost:%.4f" %c,"Accuracy:%.4f" %a)
+	print("\n Training Over")
+	y_test = y_test.reshape(y_test.size,1)
+	c,a,confmat= sess.run([cost,accuracy,predicted], {tf_x: X_test, tf_exp_y:y_test})
+	print("T_Cost:%.4f" %c,"Accuracy:%.4f" %a)
+	print(np.sum(np.absolute((confmat>THRESHOLD).astype(float)-y_test)))
+	print("\n Testing Over")
+	cm = tf.confusion_matrix(y_test,(confmat>THRESHOLD).astype(float))
+	print(sess.run(cm))
     # print(cm)
     # recall = tf.metrics.recall(labels=y_train,predictions=predicted)
     # precision = tf.metrics.precision(labels=y_train,predictions=predicted)
     # print(precision,"Precision",recall,"Recall")
-    print("\n Training Over")
-    y_test = y_test.reshape(y_test.size,1)
-    epoch = 0
-    for epoch in range(10):
-        c,a= sess.run([cost,accuracy], {tf_x: X_test, tf_exp_y:y_test})
-        cost_plot_test = np.append(cost_plot_test,c)
-        accuracy_plot_test = np.append(accuracy_plot_test,a)
-        print(epoch, "T_Cost:%.4f" %c,"Accuracy:%.4f" %a)
-    print("\n Testing Over")
-    saver = tf.train.Saver()
-    saver.save(sess, './save/model.ckpt')
-    # #To plot Cost w.r.t time
-    # plt.plot(cost_plot_train)
-    # plt.title('cost_plot_train')
-    # plt.savefig('./cost_train.png')
-    # plt.show()
-    # plt.plot(cost_plot_test)
-    # plt.title('cost_plot_test')
-    # plt.savefig('./cost_test.png')
-    # plt.show()
-    # To plot Accuracy and Cost w.r.t time
-    f, axarr = plt.subplots(2, sharex=True)
-    axarr[0].plot(cost_plot_train)
-    axarr[0].set_title('cost_plot_train')
-    axarr[1].plot(accuracy_plot_train)
-    axarr[1].set_title('Accuracy_plot_train')
-    plt.savefig('./train.png')
-    # To plot Accuracy and Cost w.r.t time
-    f, axarr = plt.subplots(2, sharex=True)
-    axarr[0].plot(cost_plot_test)
-    axarr[0].set_title('cost_plot_test')
-    axarr[1].plot(accuracy_plot_test)
-    axarr[1].set_title('Accuracy_plot_test')
-    plt.savefig('./test.png')
+
+    
