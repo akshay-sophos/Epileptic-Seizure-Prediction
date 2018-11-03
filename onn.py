4dd8433d481b22760e9cfb9c4fbd7036944dd21a
Akshay Sreekumar <akshaysreekumar1997@gmail.com>
2018-11-03 14:56:50 +0530

    Working with Confusion Matrix
    Very low F1 score

--- onn.py
+++ onn.py
@@ -3,22 +3,22 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from numpy import genfromtxt
-from sklearn.metrics import confusion_matrix
+
 
 dat = genfromtxt('chb_01_4.csv',dtype =(float), delimiter=',')
 print ('.............Data Loaded...............')
 
 learning_rate = 0.003
 THRESHOLD = 0.5
-NUM_EPISODES = 6
+NUM_EPISODES = 40
 accuracy_plot_train=[]
 cost_plot_train=[]
 
 # number of neurons in each layer
 input_num_units = 23
-hidden_num_units1 = 30
-hidden_num_units2 = 30
-hidden_num_units3 = 30
+hidden_num_units1 = 50
+hidden_num_units2 = 50
+hidden_num_units3 = 50
 output_num_units = 1
 seed = 10
 
@@ -40,6 +40,7 @@ print ('...................NN created...................')
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix,accuracy_score, f1_score, precision_score, recall_score, classification_report
 X_train, X_test, y_train, y_test = train_test_split(dat[:,:-1], dat[:,-1], test_size = 0.3, random_state = 0)
 
 with tf.Session() as sess:
@@ -52,16 +53,20 @@ with tf.Session() as sess:
 		accuracy_plot_train = np.append(accuracy_plot_train,a)
 		print(epoch, "T_Cost:%.4f" %c,"Accuracy:%.4f" %a)
 	print("\n Training Over")
+	y_pred= sess.run(predicted, {tf_x: X_train, tf_exp_y:y_train})
+	y__pred = (y_pred>THRESHOLD).astype(float)
+	print(confusion_matrix(y_train,y__pred))
+	print("Precision",precision_score(y_train, y__pred))
+	print("Recall",recall_score(y_train, y__pred))
+	print("F1",f1_score(y_train, y__pred))
+	#TEST...........................
 	y_test = y_test.reshape(y_test.size,1)
-	c,a,confmat= sess.run([cost,accuracy,predicted], {tf_x: X_test, tf_exp_y:y_test})
+	c,a,y_pred= sess.run([cost,accuracy,predicted], {tf_x: X_test, tf_exp_y:y_test})
 	print("T_Cost:%.4f" %c,"Accuracy:%.4f" %a)
-	print(np.sum(np.absolute((confmat>THRESHOLD).astype(float)-y_test)))
+	y__pred = (y_pred>THRESHOLD).astype(float)
+	#print(np.sum(np.absolute(y__pred-y_test)))
 	print("\n Testing Over")
-	cm = tf.confusion_matrix(y_test,(confmat>THRESHOLD).astype(float))
-	print(sess.run(cm))
-    # print(cm)
-    # recall = tf.metrics.recall(labels=y_train,predictions=predicted)
-    # precision = tf.metrics.precision(labels=y_train,predictions=predicted)
-    # print(precision,"Precision",recall,"Recall")
-
-    
+	print(confusion_matrix(y_test,y__pred))
+	print("Precision",precision_score(y_test, y__pred))
+	print("Recall",recall_score(y_test, y__pred))
+	print("F1",f1_score(y_test, y__pred))
