f77b2de48f0f2faa1c5bababc50994b5d3ed6c04
Akshay Sreekumar <akshaysreekumar1997@gmail.com>
2018-11-04 18:48:07 +0530

    GraphViz problem

--- lstm.py
+++ lstm.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy import genfromtxt
-dat = genfromtxt('chb_01_4.csv',dtype =(float), delimiter=',')
-#dat = np.random.rand(30,24)
+#dat = genfromtxt('chb_01_4.csv',dtype =(float), delimiter=',')
+dat = np.random.rand(30,24)>0.5
 print('............Data Loaded.................')
 
 #Variable Declaration
@@ -13,6 +13,7 @@ hip       = 23
 h1        = 60
 h2        = 60
 h3        = 60
+h4        = 60
 hop       = 1
 
 from sklearn.model_selection import train_test_split
@@ -30,29 +31,35 @@ y_train = np.expand_dims(y_train, axis=0)
 X_test = np.expand_dims(X_test, axis=0)
 y_test = np.expand_dims(y_test, axis=0).transpose()
 y_test = np.expand_dims(y_test, axis=0)
-print(np.shape(X_train))
-print(np.shape(y_train))
-print(np.shape(X_test))
-print(np.shape(y_test))
 print('............Data Normalized.............')
 
 import keras
 from keras.models import Sequential
 from keras.layers import Dense,Dropout,LSTM
+from keras.layers.normalization import BatchNormalization
 from keras.utils.vis_utils import plot_model
 import matplotlib.pyplot as plt
 from ann_visualizer.visualize import ann_viz
 
+#Should we use BatchNormalization before or after activation function??????
 
 classifier = Sequential()
+
 #Adding input layer and first hidden layer
 classifier.add(LSTM(h1, init = 'uniform',return_sequences=True, activation = 'relu',use_bias=True, input_shape = (None,hip)))
+classifier.add(BatchNormalization())
 classifier.add(Dropout(p_dropout))
 #Adding second hidden layer
 classifier.add(LSTM(h2, return_sequences=True,use_bias=True,activation = 'relu'))
+classifier.add(BatchNormalization())
 classifier.add(Dropout(p_dropout))
 #Adding third hidden layer
 classifier.add(LSTM(h3, return_sequences=True,use_bias=True,activation = 'relu'))
+classifier.add(BatchNormalization())
+classifier.add(Dropout(p_dropout))
+#4th layer
+classifier.add(LSTM(h4, return_sequences=True,use_bias=True,activation = 'relu'))
+classifier.add(BatchNormalization())
 classifier.add(Dropout(p_dropout))
 #Adding the output layer
 classifier.add(Dense(units =hop, use_bias=True,init = 'uniform', activation = 'sigmoid'))
@@ -65,11 +72,8 @@ print('............Data Trained................')
 # Predicting the Test set results
 y_pred = classifier.predict(X_train)
 y_pred = (y_pred > THRESHOLD).astype(float)
-print(y_train.shape,y_pred.shape)
 y_pred = y_pred.reshape(np.size(y_pred))
 y_train = y_train.reshape(np.size(y_train))
-print(y_train.shape,y_pred.shape)
-print(y_train,y_pred)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix,accuracy_score, f1_score, precision_score, recall_score, classification_report
 print(confusion_matrix(y_train,y_pred))
@@ -81,7 +85,7 @@ print('............Test Over...................')
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
-y_pred = (y_pred > THRESHOLD)
+y_pred = (y_pred > THRESHOLD).astype(float)
 y_pred = y_pred.reshape(np.size(y_pred))
 y_test = y_test.reshape(np.size(y_test))
 # Making the Confusion Matrix
